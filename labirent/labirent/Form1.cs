8f3aa58c1e7f67556df88a6e8b95630d358f5014
Yasin Çağatay Özbek <yasincagatayozbek@gmail.com>
2023-08-29 16:30:24 +0300

    mazeGame
    A maze game that I created using various form features in the C# language.

---
+++ labirent/labirent/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace labirent
+{
+    public partial class Form1 : Form
+    {
+        //System.Media.SoundPlayer startSound = new System.Media.SoundPlayer(@"C : Windows\Media\chord.wav");
+        //System.Media.SoundPlayer finishSound = new System.Media.SoundPlayer(@"C : Windows\Media\tada.wav");
+        public Form1()
+        {
+            InitializeComponent();
+            MoveToStart();
+        }
+
+
+        private void finishLabel_MouseEnter(object sender, EventArgs e)
+        {
+            //finishSound.Play();
+            MessageBox.Show("Congratulations!");
+            Close();
+        }
+
+        private void MoveToStart()
+        {
+            //startSound.Play();
+            Point startPoint = panel1.Location;
+            startPoint.Offset(10, 10);
+            Cursor.Position = PointToScreen(startPoint);
+        }
+
+        private void wall_MouseEnter(object sender, EventArgs e)
+        {
+            MoveToStart();
+        }
+    }
+}
